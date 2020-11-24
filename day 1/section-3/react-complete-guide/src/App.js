a1a7207302715b0e1619eecc3085ed1b04ee2966
Hasan Mahmud <tuhin.mc@gmail.com>
2020-11-24 14:12:41 +0600

    Event Handler Added

--- day 1/section-3/react-complete-guide/src/App.js
+++ day 1/section-3/react-complete-guide/src/App.js
@@ -25,8 +25,28 @@ class App extends Component {
     ]
   };
 
-  buttonClickHandler = ()=>{
-    console.log("Click Handler Working fine !");
+  buttonClickHandler = (newValue)=>{
+    this.setState({
+      persons:[
+      {
+        name : newValue,
+        age: 27
+      },
+      {
+        name : "Changed using state 2",
+        age: 28
+      },
+      {
+        name : "Changed using state 3",
+        age: 29
+      },
+      {
+        name : "Changed using state 4",
+        age: 30
+      }
+     
+      ]
+    })
   }
   render() {
     return (
@@ -34,8 +54,8 @@ class App extends Component {
         <h1> React is Working :-) !!!</h1>
         <p> I am a p 'tag' inside component. </p>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <button onClick={this.buttonClickHandler}> Change Something </button>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> Ben brought me this course! Thanks to him.</Person>
+        <button onClick={this.buttonClickHandler.bind(this,'Hasan Mahmud Tuhin')}> Change Something </button>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.buttonClickHandler.bind(this,'Tuhin')}> Ben brought me this course! Thanks to him.</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
         <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
       </div>
--- day 1/section-3/react-complete-guide/src/Person/Person.js
+++ day 1/section-3/react-complete-guide/src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const person = () =>{
-    return <p> I am from Person function component</p>;
+const person = (props) =>{
+return(
+    <div>
+     <p onClick={props.click}> I am from function component. Dynamic Content Name : {props.name}, Age : {props.age}</p>
+     <p> {props.children}</p>
+     </div>
+);
 }
 
 export default person;
